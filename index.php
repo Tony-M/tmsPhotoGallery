37dee31bdc32af1096b3f8dca34cf8da3ae33056
Морозов Антон Андреевич <aamorozov83@gmail.com>
2015-01-20 14:31:05 +0300

    Добавил обработку ошибок

--- index.php
+++ index.php
@@ -5,61 +5,74 @@
  * Date: 19.01.2015
  * Time: 13:18
  */
-
-require_once 'libs/tmsPhotoManager.php';
-
-if (file_exists('config/config.php') && is_file('config/config.php')) {
-    require_once('config/config.php');
-    if (!isset($tmsConf) || !isValidConfig($tmsConf)) {
-        error('no config data');
+try {
+    if (!file_exists('libs/tmsPhotoManager.php') || !is_file('libs/tmsPhotoManager.php')) {
+        error('Can`t start gallery. There is no main library');
         exit;
     }
+    require_once 'libs/tmsPhotoManager.php';
 
-    if (isset($tmsConf['ignore_folder']) && is_array($tmsConf['ignore_folder'])) {
-        foreach ($tmsConf['ignore_folder'] as $ignore_folder) {
-            tmsPhotoManager::setIgnoreFolderName($ignore_folder);
+    if (file_exists('config/config.php') && is_file('config/config.php')) {
+        require_once('config/config.php');
+        if (!isset($tmsConf) || !isValidConfig($tmsConf)) {
+            error('There is no config data in config/config.php ');
+            exit;
         }
+
+        if (isset($tmsConf['ignore_folder']) && is_array($tmsConf['ignore_folder'])) {
+            foreach ($tmsConf['ignore_folder'] as $ignore_folder) {
+                tmsPhotoManager::setIgnoreFolderName($ignore_folder);
+            }
+        }
+    } else {
+        error('There is no config file config/config.php');
     }
-} else {
-    error('there is no config file config/config.php');
-}
 
 
-$act = (isset($_POST['act']) ? $_POST['act'] : (isset($_GET['act']) ? $_GET['act'] : ''));
-$path = (isset($_POST['path']) ? $_POST['path'] : (isset($_GET['path']) ? $_GET['path'] : '/'));
+    $act = (isset($_POST['act']) ? $_POST['act'] : (isset($_GET['act']) ? $_GET['act'] : ''));
+    $path = (isset($_POST['path']) ? $_POST['path'] : (isset($_GET['path']) ? $_GET['path'] : '/'));
 
 //$manager = new tmsPhotoManager();
-if (!tmsPhotoManager::setRootPath($tmsConf['root_path'])) {
-    die('wrong root path');
-}
-if ($path != '/') {
-    $path = tmsPhotoManager::decode($path);
-}
-if (!tmsPhotoManager::setPath($path)) {
-    die('wrong path ' . $path);
-}
+    if (!tmsPhotoManager::setRootPath($tmsConf['root_path'])) {
+        error('Wrong root path');
+    }
+    if ($path != '/') {
+        $path = tmsPhotoManager::decode($path);
+    }
+    if (!tmsPhotoManager::setPath($path)) {
+        error('Wrong path ' . $path);
+    }
 
-switch ($act) {
-    default:
-        tmsPhotoManager::scanPath();
+    switch ($act) {
+        default:
+            tmsPhotoManager::scanPath();
 
-        require_once 'tpls/indexSuccess.php';
-        break;
-    case 'im':
-        $file = (isset($_POST['file']) ? $_POST['file'] : (isset($_GET['file']) ? $_GET['file'] : '/'));
-        tmsPhotoManager::getThumb($file);
-        exit;
-        break;
-    case 'src':
-        $file = (isset($_POST['file']) ? $_POST['file'] : (isset($_GET['file']) ? $_GET['file'] : '/'));
-        tmsPhotoManager::getImage($file);
-        exit;
-        break;
+            require_once 'tpls/indexSuccess.php';
+            break;
+        case 'im':
+            $file = (isset($_POST['file']) ? $_POST['file'] : (isset($_GET['file']) ? $_GET['file'] : '/'));
+            tmsPhotoManager::getThumb($file);
+            exit;
+            break;
+        case 'src':
+            $file = (isset($_POST['file']) ? $_POST['file'] : (isset($_GET['file']) ? $_GET['file'] : '/'));
+            tmsPhotoManager::getImage($file);
+            exit;
+            break;
+    }
+
+
+} catch (\Exception $e) {
+    die ('Critical error. Please notify your system administrator.');
 }
 
 function error($text = '')
 {
-    echo $text;
+    if (!file_exists('tpls/errorWindow.php') || !is_file('tpls/errorWindow.php')) {
+        die ('Critical Error: Can`t find Error template');
+        exit;
+    }
+    require_once 'tpls/errorWindow.php';
     exit;
 }
 
---
+++ tpls/errorWindow.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Error</title>
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/tmsPhotoManager.css">
+
+</head>
+<body style="width: 100%; height: 100%">
+            <div class="panel panel-default" style="width: 300px; height: auto; position: absolute; left:50%; margin-left: -150px; top:50%; margin-top: -70px; ">
+                <div class="panel-heading" style="text-align: left; color: red" >Error</div>
+                <div class="panel-body" style="height: 100px; color: #999 ;border: 1px solid #404040;">
+                    <?php echo $text;?>
+                </div>
+            </div>
+</body>
+</html>
